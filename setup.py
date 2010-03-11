97b1d24effc151fff804e1e6dbc8bcef20b19804
Tim Parkin <tim.parkin@jdi-associates.com>
2010-03-11 16:14:17 +0000

    cleaned up and added description

--- setup.py
+++ setup.py
@@ -1,23 +1,24 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.1'
+version = '0.1.2'
 
 setup(name='couchfti',
       version=version,
-      description="",
+      description="Xapian base full text indexing for couchdb",
       long_description="""\
 """,
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='',
-      author='Matt Goodall',
-      author_email='matt.goodall@gmail.com',
-      url='',
-      license='BSD',
+      author='Tim Parkin & Matt Goodall',
+      author_email='info@timparkin.co.uk',
+      url='http://ish.io',
+      license='',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
       install_requires=[
+          'pyparsing',
           # -*- Extra requirements: -*-
       ],
       entry_points="""
